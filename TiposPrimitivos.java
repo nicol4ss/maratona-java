3d8d915f6c29bd86b0ef5588c9a4640a52cc8a20
Nicolas Elias <nicolaselias@pm.me>
2022-08-02 23:38:26 -0300

    anotando coisas uteis

--- TiposPrimitivos.java
+++ TiposPrimitivos.java
@@ -11,16 +11,21 @@ public class TiposPrimitivos {
         boolean falso = false;// 1 bit === 0 or 1 
         char caracter =  'A';// 2 bytes === qualquer caracter da tabela ASCII ou UNICODE
         
+
         // Casting --> Compilador não entende a tipagem é resolvido:
         // As iniciais dos tipos são considerados como especificação
         float salarioFloat2 = 2000.0f;//  Colocando um f especificando q é float
         double salarioDouble2 = 2000.0d;//  Ou um d especificando que é double
         
+
         // A Maneira de "forçar" colocar um tipo em outra da seguinte forma:
         //O L no final do numero long especifica que ele é um long
         // E quer converter o long em int para fazer caber no tamanho de um int
         int idade2 = (int) 100000000000000L;// Porem isso vai ser cortado uns bytes
         float salarioFloat3 = (float) 4000.0d;// é possivel tb mas se perde bytes
+
+        // String é uma classe
+        String nome = "Treinando tipos primitivos";
 
         System.out.println(idade);
         System.out.println(numeroGrande);
@@ -35,5 +40,6 @@ public class TiposPrimitivos {
         System.out.println(salarioDouble2);
         System.out.println(idade2);
         System.out.println(salarioFloat3);
+        System.out.println(nome);
     }
 }
